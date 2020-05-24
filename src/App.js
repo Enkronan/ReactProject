ac4a8e1e4b0c8f9ed738fc04be99c266ba51c26b
enkronan <robin@tempura.se>
2020-05-24 15:01:36 +0200

    type: feature
    mapping over a bunch of users to create tweets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, {useState} from "react";
 import "./App.css"
+import Tweet from "./Tweet";
 
 function App(){
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count +1);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    {name: "Robin", message: "Hello there"},
+    {name: "Elin", message: "Hiya"},
+    {name: "Dog", message: "Woof!"}
+  ]);
 
   return (
     <div className="App">
-      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
