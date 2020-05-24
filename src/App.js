a432e79fa918fc266184c1fd06c17830d098de9e
enkronan <robin@tempura.se>
2020-05-24 14:33:03 +0200

    type: feature
    added a component for tweets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Hello from "./sayHello";
+import Tweet from "./Tweet"
 
 function App(){
 
-
-
   return (
     <div>
-      <h1>This is the App component</h1>
-      <Hello />
+      <h1>Hello React</h1>
+      <Tweet/>
     </div>
   );
 }
